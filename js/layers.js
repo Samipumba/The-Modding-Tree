5d97f992776befe2f4a45b6a149bb0a977145deb
Samipumba <samuelnoriegasoria@gmail.com>
2023-12-11 15:27:52 -0300

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -24,13 +24,13 @@ addLayer("Z", {
     hotkeys: [
         {key: "p", description: "P: Reset for prestige points", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
     ],
-    layerShown(){return true}
-})
-upgrades: {
-    11; {
-        description: "Blah",
-        cost; new Decimal(100),
+    layerShown(){return true},
+    upgrades: {
+        11: {
+            description: "Blah",
+            cost: new Decimal(100),
+            etc
+        },
         etc
     }
-    etc
-}
+})
